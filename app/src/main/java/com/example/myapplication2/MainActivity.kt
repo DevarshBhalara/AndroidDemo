c9f382f7268b01190a13f2c4379d7886df54b838
DevarshBhalara <devarshbhalara3072>
2023-02-20 14:37:55 +0530

    First

---
+++ app/src/main/java/com/example/myapplication2/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toolbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById(R.id.toolbar) as Toolbar? ;
+
+
+    }
+
+
+}
